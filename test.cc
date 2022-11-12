db33f277838c56fe46c35cd17e79dcd499d3b736
Scott Yeager <dsyeager@thehouseofyeager.com>
2022-11-12 06:33:08 -0800

    initial commit

---
+++ test.cc
@@ -0,0 +1,24 @@
+
+
+#include <iostream>
+
+class fifo_mutex
+{
+public:
+
+};
+
+class scoped_fifo_mutex
+{
+public:
+
+};
+
+using namespace std;
+
+
+int main (int argc, char** argv)
+{
+    cout << "hello world" << endl;
+
+}
